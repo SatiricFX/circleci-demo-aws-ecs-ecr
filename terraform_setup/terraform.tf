fe4890fed38fe6d9c4d367d52e78c8c57180be71
SatiricFX <mr.quentin.johnston@gmail.com>
2020-02-07 17:42:18 -0500

    Update config.

--- terraform_setup/terraform.tf
+++ terraform_setup/terraform.tf
@@ -298,12 +298,12 @@ resource "aws_ecs_service" "ecs-service" {
   name            = "ecs-service"
   iam_role        = "${aws_iam_role.ecs-service-role.name}"
   cluster         = "${aws_ecs_cluster.ecs-cluster.id}"
-  	task_definition = "${aws_ecs_task_definition.ecs_task_definition.family}:${max("${aws_ecs_task_definition.ecs_task_definition.revision}", "${data.aws_ecs_task_definition.ecs_task_definition.revision}")}"
-  	desired_count   = 1
+  task_definition = "${aws_ecs_task_definition.ecs_task_definition.arn}"
+  desired_count   = 1
 
-  	load_balancer {
-    	target_group_arn  = "${aws_alb_target_group.ecs-target-group.arn}"
-    	container_port    = 80
-    	container_name    = "cwvlug_circleci_demo"
+  load_balancer {
+   	target_group_arn  = "${aws_alb_target_group.ecs-target-group.arn}"
+   	container_port    = 80
+   	container_name    = "cwvlug_circleci_demo"
 	}
 }
