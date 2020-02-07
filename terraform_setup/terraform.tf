9d90ff98e34c7de288f77dbb9d4257b77ca13b9c
SatiricFX <mr.quentin.johnston@gmail.com>
2020-02-07 17:20:08 -0500

    Add service.

--- terraform_setup/terraform.tf
+++ terraform_setup/terraform.tf
@@ -289,3 +289,18 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
 ]
 DEFINITION
 }
+
+resource "aws_ecs_service" "ecs-service" {
+  name            = "ecs-service"
+  iam_role        = "${aws_iam_role.ecs-service-role.name}"
+  cluster         = "${aws_ecs_cluster.ecs-cluster.id}"
+  	task_definition = "${aws_ecs_task_definition.cwvlug_circleci_demo.family}:${max("${aws_ecs_task_definition.cwvlug_circleci_demo.revision}", "${data.aws_ecs_task_definition.cwvlug_circleci_demo.revision}")}"
+  	desired_count   = 1
+
+  	load_balancer {
+    	target_group_arn  = "${aws_alb_target_group.ecs-target-group.arn}"
+    	container_port    = 80
+    	container_name    = "cwvlug_circleci_demo"
+	}
+}
+}
