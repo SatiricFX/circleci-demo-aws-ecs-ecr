64c3d0c9df0ca21be54ebcda3e0a32dda4d96b4c
SatiricFX <mr.quentin.johnston@gmail.com>
2020-02-07 19:13:43 -0500

    Update tf variables.

--- terraform_setup/variables.tf
+++ terraform_setup/variables.tf
@@ -1,8 +1,11 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_account_id" {}
-variable "aws_region" {}
-variable "aws_resource_prefix" {}
-variable "aws_vpc_cidr_block" {}
-variable "aws_vpc_public_sn_01_cidr_block" {}
-variable "aws_vpc_public_sn_02_cidr_block" {}
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
+#variable "aws_account_id" {}
+#variable "aws_region" {}
+#ariable "aws_resource_prefix" {}
+#variable "aws_vpc_cidr_block" {}
+#variable "aws_vpc_public_sn_01_cidr_block" {}
+#variable "aws_vpc_public_sn_02_cidr_block" {}
+
+aws_vpc_public_sn_01_cidr_block = "10.0.1.0/24"
+aws_vpc_public_sn_02_cidr_block = "10.0.2.0/24"
