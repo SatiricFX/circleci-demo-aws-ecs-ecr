5878a4d33e37794b5078dbf2a92d6dc84659acfc
SatiricFX <mr.quentin.johnston@gmail.com>
2020-02-07 19:17:58 -0500

    Update tf variables.

--- terraform_setup/variables.tf
+++ terraform_setup/variables.tf
@@ -1,7 +1,3 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_account_id" {}
-variable "aws_region" {}
 variable "aws_resource_prefix" {}
 variable "aws_vpc_cidr_block" {}
 variable "aws_vpc_public_sn_01_cidr_block" {}
