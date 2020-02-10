7aab41ed90e6f90483195a90b53e748517b369b3
SatiricFX <mr.quentin.johnston@gmail.com>
2020-02-10 16:53:21 -0500

    Add bracket for TF variable.

--- terraform_setup/variables.tf
+++ terraform_setup/variables.tf
@@ -2,4 +2,4 @@ variable "aws_resource_prefix" {}
 variable "aws_vpc_cidr_block" {}
 variable "aws_vpc_public_sn_01_cidr_block" {}
 variable "aws_vpc_public_sn_02_cidr_block" {}
-variable "aws_region"
+variable "aws_region" {}
