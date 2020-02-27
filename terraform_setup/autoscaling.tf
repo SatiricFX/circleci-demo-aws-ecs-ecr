5d754167ec78a09def6ba663c39431d282eb1840
SatiricFX <39910476+SatiricFX@users.noreply.github.com>
2020-02-27 17:05:14 -0500

    Update autoscaling.tf

--- terraform_setup/autoscaling.tf
+++ terraform_setup/autoscaling.tf
@@ -1,11 +1,6 @@
-resource "aws_key_pair" "cwvlug-key-pair" {
-  key_name   = "cwvlug-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
-}
-
 resource "aws_launch_configuration" "cwvlug-ecs-launch-configuration" {
     name                        = "cwvlug-ecs-launch-configuration"
-    image_id                    = "ami-0f81924348bcd01a1"
+    image_id                    = "ami-098616968d61e549e"
     instance_type               = "t2.micro"
     iam_instance_profile        = "${aws_iam_instance_profile.cwvlug-ecs-instance-profile.id}"
 
@@ -21,7 +16,6 @@ resource "aws_launch_configuration" "cwvlug-ecs-launch-configuration" {
 
     security_groups             = ["${aws_security_group.cwvlug_public_sg.id}"]
     associate_public_ip_address = "true"
-    key_name                    = "cwvlug-key"
     user_data                   = <<EOF
                                   #!/bin/bash
                                   echo ECS_CLUSTER=cwvlug-ecs-cluster >> /etc/ecs/ecs.config
