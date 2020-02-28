5f836891cb538b346722cee671d56a24a811489c
SatiricFX <39910476+SatiricFX@users.noreply.github.com>
2020-02-27 21:00:46 -0500

    Update autoscaling.tf

--- terraform_setup/autoscaling.tf
+++ terraform_setup/autoscaling.tf
@@ -2,7 +2,7 @@ resource "aws_launch_configuration" "cwvlug-ecs-launch-configuration" {
     name                        = "cwvlug-ecs-launch-configuration"
     image_id                    = "ami-098616968d61e549e"
     instance_type               = "t2.micro"
-    iam_instance_profile        = "aws_iam_instance_profile.cwvlug-ecs-instance-profile.id"
+    iam_instance_profile        = "${aws_iam_instance_profile.cwvlug-ecs-instance-profile.id}"
 
     root_block_device {
       volume_type = "standard"
@@ -24,7 +24,7 @@ resource "aws_autoscaling_group" "cwvlug-ecs-autoscaling-group" {
     min_size                    = "0"
     desired_capacity            = "1"
     vpc_zone_identifier         = ["${aws_subnet.cwvlug_public_sn_01.id}", "${aws_subnet.cwvlug_public_sn_02.id}"]
-    launch_configuration        = "aws_launch_configuration.cwvlug-ecs-launch-configuration.name"
+    launch_configuration        = "${aws_launch_configuration.cwvlug-ecs-launch-configuration.name}"
     health_check_type           = "ELB"
     tags = [
     {
