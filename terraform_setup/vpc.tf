24806180e71f625e821b20177030b70741b3b4f0
SatiricFX <39910476+SatiricFX@users.noreply.github.com>
2020-02-27 20:41:09 -0500

    Update vpc.tf

--- terraform_setup/vpc.tf
+++ terraform_setup/vpc.tf
@@ -77,25 +77,10 @@ resource "aws_security_group" "cwvlug_public_sg" {
   description = "Public access security group"
   vpc_id = "${aws_vpc.cwvlug_vpc.id}"
 
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"]
-  }
 
   ingress {
     from_port = 80
     to_port = 80
-    protocol = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 8080
-    to_port = 8080
     protocol = "tcp"
     cidr_blocks = [
       "0.0.0.0/0"]
